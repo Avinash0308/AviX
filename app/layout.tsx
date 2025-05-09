9e8b74970872839df7de16e40460466e49389b47
Avinash Agrawal <avinash03082003@gmail.com>
2025-05-09 15:49:07 +0530

    Changed title from genius to genius.ai and removed the copyright free text.

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import './globals.css'
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'NEX',
+  title: 'Genius.AI',
   description: 'AI Platform',
 }
 
